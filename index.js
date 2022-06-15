1c713b22c488a3d6d0117e3cfb22575d99877f57
cristina <cristinaaran89@gmail.com>
2022-06-15 16:10:24 +0200

    fondo html

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ import { Juego } from './clases/Juego.js'
 document.getElementById('canvas').style.display = 'none';
 document.getElementById("marcador").style.display = 'none'
 document.getElementById("marcadorCafe").style.display = 'none'
+const audio = new Audio();
+audio.src='./musica/intro.mp3'
 
 
 console.log('index')
 window.addEventListener('load', () => { 
+    
+    audio.play()
     let start = document.getElementById('submit');
     start.addEventListener('click', () => {
+        audio.pause();
         document.getElementById('pantalla__principal').style.display = 'none';
+        document.getElementById("marcador").style.display = 'block';
         document.getElementById('canvas').style.display = 'block';
         const canvas = document.getElementById('canvas');
         const ctx = canvas.getContext('2d');
